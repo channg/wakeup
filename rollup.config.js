bda632eb94806cdb4b2b332e80d75373bd39f839
unknown <xuezhiwen@corp.pandatv.com>
2018-08-17 10:59:10 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import autoprefixer from 'autoprefixer'
+import postcss from 'rollup-plugin-postcss'
+import vars from 'postcss-simple-vars'
+import preset from 'postcss-preset-env'
+export default {
+  input: 'index.js',
+  output: {
+    file: 'bundle.js',
+    format: 'umd',
+    name:'some'
+  },
+  plugins: [
+    postcss({
+      plugins: [
+        vars(),
+        preset(),
+        autoprefixer({browsers: ['> 0.001%', 'not ie < 9'],}),
+      ],
+    }),
+    resolve(),
+    commonjs({
+      include: './**'
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    serve(),
+    livereload()
+  ]
+}
